ed5b4cd95ef7c380ad26869e4fcba1dbf3eff193
mared-dev <100310843+mared-dev@users.noreply.github.com>
2022-03-23 15:40:19 +0400

    finishing post details

--- lib/screens/PostDetails/post_details_screen.dart
+++ lib/screens/PostDetails/post_details_screen.dart
@@ -1,14 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mared_social/constants/colors.dart';
+import 'package:mared_social/mangers/user_info_manger.dart';
 import 'package:mared_social/widgets/reusable/feed_post_item.dart';
 import 'package:mared_social/widgets/reusable/simple_appbar_with_back.dart';
 
-class PostDetailsScreen extends StatelessWidget {
+class PostDetailsScreen extends StatefulWidget {
   final documentSnapshot;
 
   const PostDetailsScreen({Key? key, this.documentSnapshot}) : super(key: key);
+
+  @override
+  State<PostDetailsScreen> createState() => _PostDetailsScreenState();
+}
+
+class _PostDetailsScreenState extends State<PostDetailsScreen> {
+  bool shouldUseSentData = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    shouldUseSentData = (widget.documentSnapshot != null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,8 +42,33 @@ class PostDetailsScreen extends StatelessWidget {
       }),
       body: Padding(
         padding: EdgeInsets.only(top: 16.h),
-        child: FeedPostItem(
-          documentSnapshot: documentSnapshot,
+        child: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(UserInfoManger.getUserId())
+              .collection("posts")
+              .doc(widget.documentSnapshot['postid'])
+              .snapshots(),
+          builder: (context, postDocSnap) {
+            if (postDocSnap.connectionState == ConnectionState.waiting ||
+                !postDocSnap.hasData) {
+              if (shouldUseSentData) {
+                shouldUseSentData = false;
+                return FeedPostItem(
+                  documentSnapshot: widget.documentSnapshot,
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              print('!!!!!!!!!!!!!');
+              print(postDocSnap.data);
+              return FeedPostItem(
+                documentSnapshot: postDocSnap.data,
+              );
+            }
+          },
         ),
       ),
     );
